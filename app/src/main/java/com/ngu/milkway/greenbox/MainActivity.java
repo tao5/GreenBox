58e18e6009b8243decb12960d923a96d8f13c0b2
tao5 <xutao.ngu@gmail.com>
2016-06-15 21:26:07 +0800

    add tao

--- app/src/main/java/com/ngu/milkway/greenbox/MainActivity.java
+++ app/src/main/java/com/ngu/milkway/greenbox/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("ngu", "add by eclipsedMs !");
         Log.d("ngu", "add by eclipsedMs2 ");
         Log.d("ngu", "add by tao ");
+        Log.d("ngu", "add by tao ");
 
     }
 }
